96fe1f0e01d1f566f89ab93bb570811d8521628c
jspc <james@zero-internet.org.uk>
2012-11-03 03:59:59 +0000

    Handle hosts in either of the two states we give a fuck about

--- lib/ddis.rb
+++ lib/ddis.rb
@@ -4,6 +4,7 @@
 
 require 'netaddr'
 require 'resolv'
+require 'net/ping/icmp'
 
 require 'ddis/ip_iterate.rb'
 require 'ddis/store_ips.rb'
@@ -14,12 +15,14 @@ class DDis
   def initialize range unassigned
     @unass    = unassigned
     @resolv   = Resolv.new
+    @ping     = Net::Ping::ICMP.new nil, nil, 1
 
     ip_range  = NetAddr::CIDR.create range
     @base     = ip_range.base
     @size     = ip_range.size
     @iterator = Iterator.new @base, @size
     @db       = StoreIPs.new
+
   end
 
   def test_ip ip
@@ -29,10 +32,16 @@ class DDis
       return nil
     end
     
-    up = 
+    @ping.host = ip
     
-    @db.up_no_dns( ip ) if 
-
+    @db.up_no_dns( ip )     if ( @ping.ping? and
+                                 hostname =~ @unass
+                                 )
+    @db.down_with_dns( ip ) if ( !@ping.ping? and
+                                 hostname !~ @unass
+                                 )
+    
   end
   
+end
   
