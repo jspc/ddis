da61ff0df7703695a23b4f1278902a805ff16c50
jspc <james@zero-internet.org.uk>
2012-11-04 00:59:57 +0000

    Do something sensible with the ddis executable

--- lib/ddis.rb
+++ lib/ddis.rb
@@ -12,7 +12,9 @@ require 'ddis/store_ips.rb'
 class DDis
   attr_accessor :iterator
   
-  def initialize range, unassigned
+  def initialize range, unassigned, verbose=nil
+    @verbose  = true if verbose
+
     @unass    = unassigned
     @resolv   = Resolv.new
     @ping     = Net::Ping::ICMP.new nil, nil, 1
@@ -29,7 +31,7 @@ class DDis
     begin
       hostname = @resolv.getname ip
     rescue
-      puts "Couldn't get host for #{ip}"
+      warn "Couldn't get host for #{ip}"
       return nil
     end
     
@@ -37,10 +39,12 @@ class DDis
     up = @ping.ping?
 
     if up and hostname =~ /#{@unass}/
-       @db.up_no_dns( ip )
+      warn "#{ip} is up yet has an invalid hostname" if @verbose
+      @db.up_no_dns( ip )
     end
 
     if not up and hostname !~ /#{@unass}/
+      warn "#{ip} is down yet has a hostname set" if @verbose
       @db.down_with_dns( ip )
     end
   end
