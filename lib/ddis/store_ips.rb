87334f9258268fdc73238fd591775de752de703c
jspc <james@zero-internet.org.uk>
2012-11-06 22:30:02 +0000

    Do the thing

--- lib/ddis/store_ips.rb
+++ lib/ddis/store_ips.rb
@@ -9,25 +9,28 @@ class StoreIPs
   
   def initialize
     @red = Redis.new
-    @db  = Date.now.to_s
-    1
+    @db  = Date.today.to_s
+    @red.sadd "ip.dates", @db
+    @red.save
   end
 
   def up_no_dns ip
-    @red.srem "ip.@db.down", ip
-    @red.sadd "ip.@db.up", ip
+    @red.srem "ip.#{@db}.down", ip
+    @red.sadd "ip.#{@db}.up", ip
+    @red.save
   end
 
   def down_with_dns ip
-    @red.srem "ip.@db.up", ip
-    @red.sadd "ip.@db.down", ip
+    @red.srem "ip.#{@db}.up", ip
+    @red.sadd "ip.#{@db}.down", ip
+    @red.save
   end
 
   def dump
     0 if @red.sinter "ip.@db.down", "ip.@db.up"
-    {
-      'up'    => @red.smembers "ip.@db.up",
-      'down'  => @red.smembers "ip.@db.down",
+    { 
+      :up    => @red.smembers "ip.#{@db}.up" , 
+      :down  => @red.smembers "ip.#{@db}.down" , 
     }
   end
 
